40240190431cb26c70ad40e927ae231f27b5ef56
Christopher Mark Gore <cgore@cgore.com>
2021-07-02 20:18:24 -0500

    four cubes and a point light

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ pub mod startup;
 
 fn main() {
     App::build()
+        .insert_resource(Msaa { samples: 4 })
         .add_plugins(DefaultPlugins)
         .add_startup_system(startup::startup.system())
         .run();
--- src/startup.rs
+++ src/startup.rs
@@ -2,10 +2,10 @@ use bevy::prelude::*;
 
 use super::camera;
 
-fn add_plane(
+fn plane(
     meshes: &mut ResMut<Assets<Mesh>>,
     materials: &mut ResMut<Assets<StandardMaterial>>
-) -> bevy::prelude::PbrBundle {
+) -> PbrBundle {
     PbrBundle {
         mesh: meshes.add(Mesh::from(shape::Plane { size: 5.0 })),
         material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
@@ -13,7 +13,26 @@ fn add_plane(
     }
 }
 
+fn cube(
+    color: [f32;3],
+    coords: [f32;3],
+    meshes: &mut ResMut<Assets<Mesh>>,
+    materials: &mut ResMut<Assets<StandardMaterial>>
+) -> PbrBundle {
+    PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+        material: materials.add(Color::rgb(color[0],color[1],color[2]).into()),
+        transform: Transform::from_xyz(coords[0],coords[1],coords[2]),
+        ..Default::default()
+    }
+}
 
+fn light() -> LightBundle {
+    LightBundle {
+        transform: Transform::from_xyz(3.0, 8.0, 5.0),
+        ..Default::default()
+    }
+}
 
 pub fn startup(
     mut commands: Commands,
@@ -25,6 +44,22 @@ pub fn startup(
     let camera = camera::camera();
     commands.spawn_bundle(camera);
 
-    commands.spawn_bundle(add_plane(&mut meshes, &mut materials));
-   
+    commands.spawn_bundle(plane(&mut meshes, &mut materials));
+    commands.spawn_bundle(cube([0.8, 0.7, 0.6],
+                               [1.5, 0.5, 1.5],
+                               &mut meshes,
+                               &mut materials));
+    commands.spawn_bundle(cube([0.8, 0.7, 0.6],
+                               [1.5, 0.5, -1.5],
+                               &mut meshes,
+                               &mut materials));
+    commands.spawn_bundle(cube([0.8, 0.7, 0.6],
+                               [-1.5, 0.5, 1.5],
+                               &mut meshes,
+                               &mut materials));
+    commands.spawn_bundle(cube([0.8, 0.7, 0.6],
+                               [-1.5, 0.5, -1.5],
+                               &mut meshes,
+                               &mut materials));
+    commands.spawn_bundle(light());
 }
