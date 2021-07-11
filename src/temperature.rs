ab042ea591046174b62af003af62bc1a6b74865c
Christopher Mark Gore <cgore@cgore.com>
2021-07-11 00:28:09 -0500

    Keeping track of the scale we're wanting the temperature in

--- src/temperature.rs
+++ src/temperature.rs
@@ -1,30 +1,71 @@
 #[derive(PartialEq)]
+enum Scale {
+    Celsius,
+    Fahrenheit,
+    Kelvin,
+    Rankine
+}
+
+#[derive(PartialEq)]
 pub struct Temperature {
-    kelvin: f32
+    kelvin: f32,
+    scale: Scale
 }
 
 impl Temperature {
     pub fn kelvin(kelvin: f32) -> Temperature {
         Temperature {
-            kelvin: kelvin
+            kelvin: kelvin,
+            scale: Scale::Kelvin
         }
     }
 
     pub fn celsius(celsius: f32) -> Temperature {
         Temperature {
-            kelvin: celsius + 273.15
+            kelvin: celsius + 273.15,
+            scale: Scale::Celsius
         }
     }
 
     pub fn fahrenheit(fahrenheit: f32) -> Temperature {
         Temperature {
-            kelvin: (fahrenheit + 459.67) * (5.0/9.0)
+            kelvin: (fahrenheit + 459.67) * (5.0/9.0),
+            scale: Scale::Fahrenheit
         }
     }
 
     pub fn rankine(rankine: f32) -> Temperature {
         Temperature {
-            kelvin: (rankine + 459.67)
+            kelvin: (rankine + 459.67),
+            scale: Scale::Rankine
+        }
+    }
+
+    pub fn to_kelvin(temperature: Temperature) -> Temperature {
+        Temperature {
+            kelvin: temperature.kelvin,
+            scale: Scale::Kelvin
+        }
+    }
+
+    pub fn to_celsius(temperature: Temperature) -> Temperature {
+        Temperature {
+            kelvin: temperature.kelvin,
+            scale: Scale::Celsius
+        }
+    }
+
+    pub fn to_fahrenheit(temperature: Temperature) -> Temperature {
+        Temperature {
+            kelvin: temperature.kelvin,
+            scale: Scale::Fahrenheit
+        }
+    }
+
+    pub fn to_rankine(temperature: Temperature) -> Temperature {
+        Temperature {
+            kelvin: temperature.kelvin,
+            scale: Scale::Rankine
         }
     }
 }
