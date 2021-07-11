197a5ddd0774b59c3561e07e167cfc8a28ea9658
Christopher Mark Gore <cgore@cgore.com>
2021-07-11 15:21:03 -0500

    Some more work on Mass

--- src/mass.rs
+++ src/mass.rs
@@ -4,10 +4,9 @@
 // Therefore we feel f64 is acceptable here.
 
 pub enum Scale {
-    Gram,
-    KiloGram,
-    Pound,
-    SolarMass
+    Gram, Kilogram,
+    Ounce, Pound,
+    EarthMass, SolarMass
 }
 
 pub struct Mass {
@@ -25,5 +24,66 @@ impl Mass {
             grams: grams,
             scale: Scale::Gram
         }
+    }
+
+    pub fn kilograms(kilograms: f64) -> Mass {
+        Mass {
+            grams: kilograms / 1_000.0,
+            scale: Scale::Kilogram
+        }
+    }
+
+    pub fn ounces(ounces: f64) -> Mass {
+        Mass {
+            grams: ounces * 28.34952,
+            scale: Scale::Ounce
+        }
+    }
+
+    pub fn pounds(pounds: f64) -> Mass {
+        Mass {
+            grams: pounds * 453.5924,
+            scale: Scale::Pound
+        }
+    }
+
+    pub fn earth_masses(earth_masses: f64) -> Mass {
+        Mass {
+            grams: earth_masses * 5.9722e24 * 1_000.0,
+            scale: Scale::EarthMass
+        }
+    }
+
+    pub fn solar_masses(solar_masses: f64) -> Mass {
+        Mass {
+            grams: solar_masses * 1.98847e30 * 1_000.0,
+            scale: Scale::SolarMass
+        }
+    }
+
+    pub fn g(g: f64) -> Mass {
+        Mass::grams(g)
+    }
+
+    pub fn kg(kg: f64) -> Mass {
+        Mass::kilograms(kg)
+    }
+
+    pub fn oz(oz: f64) -> Mass {
+        Mass::ounces(oz)
+    }
+
+    pub fn lb(lb: f64) -> Mass {
+        Mass::pounds(lb)
+    }
+
+    #[allow(non_snake_case)]
+    pub fn Mearth(earth_masses: f64) -> Mass {
+        Mass::earth_masses(earth_masses)
+    }
+
+    #[allow(non_snake_case)]
+    pub fn Msol(solar_masses: f64) -> Mass {
+        Mass::solar_masses(solar_masses)
     }
 }
